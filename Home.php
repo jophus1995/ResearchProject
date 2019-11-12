f7427e6e7bbb2d1be79b322eeaca54eaa8f57b92
Andy Luong <31705702+andyyluong@users.noreply.github.com>
2019-11-12 18:29:25 -0500

    Config, Home, and Faculty PHP Files

--- Home.php
+++ Home.php
@@ -1,12 +1,63 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title> UMW Faculty & Student Research Website</title>
+  <meta charset="utf-8">
+  <!-- Bootstrap 4; Sets initial zoom level and sets the width to the screen width of the viewing device -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <!-- jQuery library -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <!-- Popper JS -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <!-- Latest compiled JavaScript -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  <!-- Imports Google Font Open-Sans -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
+  <!-- General Stylesheet Link -->
+  <link rel="stylesheet" type="text/css" href="../css/BootstrapTemplate.css">
 </head>
 
 <body>
+<!-- Container for header -->
+<div class="container-fluid"> <!-- container-fluid is a full width container. it scales to the screen width -->
+    <div class="row header"> <!-- each row can contain up to 12 columns. no matter what, all col must add up to 12 -->
+      <div class="col-sm-1">
+        <!-- Dropdown Button -->
+        <div class="dropdown">
+          <button type="button" class="btn" data-toggle="dropdown">
+            <img src='../inc/burger-menu.jpg' class='img-fluid'>
+          </button>
+          <div class="dropdown-menu">
+            <a class="dropdown-item" href="http://35.245.253.27/Home.html"><h3>Home</h3></a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="http://35.245.253.27/FacultyPage.html"><h3>Faculty Page</h3></a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="http://35.245.253.27/StudentSubmission.php"><h3>Browse Submissions</h3></a>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-6"><h1>Faculty & Student<br> Research</h1></div>
+      <div class="col-sm-3"></div>
+      <!-- Login Button -->
+      <div class="col-sm-1">
+        <a class="btn btn-primary" href="http://35.245.253.27/Login.html"><h4>Login</h4></a>
+      </div>
+      <div class="col-sm-1"></div>
+    </div>
+</div>
 
-    <h1> Student Research Home Page </h1>
+<div class="jumbotron main"> <!-- jumbotron acts like a big screen, and anything inside of it is fit to its dimensions -->
+  <div class="container-fluid"> <!-- normally this would watch screen-width, but since it's in a jumbotron, it only matches jumbotron width -->
+    <div class="row">
+      <div class="col-sm-1"><h1>Home</h1></div>
+      <div class="col-sm-2"></div>
+      <div class="col-sm-9"></div>
+    </div>
+  </div>
+</div>
+
+
 
 </body>
 
---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+$mysql_host = '35.238.146.54';
+$mysql_user = 'umwresearch';
+$mysql_pass = 'csumw2019';
+$mysql_db = 'studentfacultyDB';
+
+//SQL Connection
+$con = mysqli_connect($mysql_host, $mysql_user, $mysql_pass, $mysql_db);
+
+
+?>
